8171a7dda3b6642c2de5a0034cd829194b0f4474
Lala <hajiyeva.lalah@gmail.com>
2019-12-09 12:31:17 +0400

    -- spring, annotations

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,39 +1,21 @@
 package app;
 
+import app.entity.Person;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
 /**
- * http://localhost:8080/users
+ * http://localhost:8081/users
  */
-@RestController
 @SpringBootApplication
 public class Application {
+    public static void main(String[] args) {
 
-  static class Person {
-    private String name;
-    Person(String name) {
-      this.name = name;
+        SpringApplication.run(Application.class);
     }
-    public String getName() {
-      return name;
-    }
-  }
 
-  public static void main(String[] args) {
-    SpringApplication.run(Application.class);
-  }
-
-  @GetMapping("/users")
-  public List<Person> btdf () {
-    return Arrays.asList(
-        new Person("Jim"),
-        new Person("John"));
-  }
 
 }
